bb148cb1eb1e23aa7b1e75b13e99dc195decf173
B Water <brennw47@gmail.com>
2024-06-05 10:21:53 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,17 @@
 // Initiate the router
 const router = require('express').Router();
+// Import the userRoutes
+const userRoutes = require('./userRoutes');
+// Import the postRoutes
+const postRoutes = require('./postRoutes');
+// Import the commentRoutes
+const commentRoutes = require('./commentRoutes');
+// Use the userRoutes
+router.use('/users', userRoutes);
+// Use the postRoutes
+router.use('/posts', postRoutes);
+// Use the commentRoutes
+router.use('/comments', commentRoutes);
 
-// Import all of the API routes from /api/index.js
+// Export all of the API routes from /api/index.js
 module.exports = router;
