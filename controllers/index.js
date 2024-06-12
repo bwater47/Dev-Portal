cb1f5840c62cf0ee1ce7fe3a5d256c17435e8d75
B Water <brennw47@gmail.com>
2024-06-12 11:27:34 -0500

    fix(RENDER) Fixed homeroutes being stuck in GitHub as all lowercase to lowercase locally
    fix(RENDER) Fixed homeroutes being stuck in GitHub as all lowercase to lowercase locally
    
    This should fix the render deployment.

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@
 // It imports the express router and the api routes and home routes.
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes.js');
+const homeroutes = require('./homeroutes.js');
 const dashboardRoutes = require('./dashboardRoutes.js');
 // It then uses the router to use the home routes and api routes. Finally, it exports the router.
-router.use('/', homeRoutes);
+router.use('/', homeroutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 module.exports = router;
